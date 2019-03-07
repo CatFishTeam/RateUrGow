0b12963eb04fd7aab2bf7d1ef56f54276bf70b6d
Baldrani <mael.mayon@free.fr>
2019-03-07 11:11:38 +0100

    Fix Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style/sass/main.sass';
-import LoginFormContainer from "./Containers/LoginFormContainer";
-import RegisterFormContainer from "./Containers/RegisterFormContainer";
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-
+import NavbarContainer from "./containers/NavbarContainer";
+import LoginFormContainer from "./containers/LoginFormContainer";
+import RegisterFormContainer from "./containers/RegisterFormContainer";
 
 function Index() {
     return <h2>Home</h2>;
@@ -12,40 +12,14 @@ function Index() {
 function AppRouter() {
     return (
         <Router>
-        <div>
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <Link className={"navbar-item"} to="/">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-                    </Link>
-
-                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                       data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
+            <React.Fragment>
+                <NavbarContainer/>
+                <div className="container">
+                    <Route path="/login/" component={LoginFormContainer}/>
+                    <Route path="/register/" component={RegisterFormContainer}/>
                 </div>
-
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <Link className={"navbar-item"} to="/">Home</Link>
-                    </div>
-
-                    <div className="navbar-end">
-                        <Link className={"navbar-item"} to="/login/">Se connecter</Link>
-                        <Link className={"navbar-item"} to="/register/">Creer un compte</Link>
-                    </div>
-                </div>
-            </nav>
-
-            <div className="container">
-                <Route path="/" exact component={Index}/>
-                <Route path="/login/" component={LoginFormContainer}/>
-                <Route path="/register/" component={RegisterFormContainer}/>
-            </div>
-        </div>
-    </Router>);
+            </React.Fragment>
+        </Router>);
 }
 
 export default AppRouter;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,48 +1,41 @@
 import React, {PureComponent} from 'react'
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import LoginFormContainer from "../Containers/LoginFormContainer";
-import RegisterFormContainer from "../Containers/RegisterFormContainer";
+import {Link, Route} from "react-router-dom";
+import RegisterFormContainer from "../containers/RegisterFormContainer";
+import LoginFormContainer from "../containers/LoginFormContainer"
 
 class Navbar extends PureComponent {
+
     render() {
         return (
-            <Router>
-                <div>
-                    <nav className="navbar" role="navigation" aria-label="main navigation">
-                        <div className="navbar-brand">
-                            <Link className={"navbar-item"} to="/">
-                                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-                            </Link>
+            <div>
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <Link className={"navbar-item"} to="/">
+                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                        </Link>
 
-                            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                               data-target="navbarBasicExample">
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                            </a>
-                        </div>
-
-                        <div id="navbarBasicExample" className="navbar-menu">
-                            <div className="navbar-start">
-                                <Link className={"navbar-item"} to="/">Home</Link>
-                            </div>
-
-                            <div className="navbar-end">
-                                <Link className={"navbar-item"} to="/login/">Se connecter</Link>
-                                <Link className={"navbar-item"} to="/register/">Creer un compte</Link>
-                            </div>
-                        </div>
-                    </nav>
-
-                    <div className="container">
-                        <Route path="/" exact component={Index}/>
-                        <Route path="/login/" component={LoginFormContainer}/>
-                        <Route path="/register/" component={RegisterFormContainer}/>
+                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                           data-target="navbarBasicExample">
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
                     </div>
-                </div>
-            </Router>
+
+                    <div id="navbarBasicExample" className="navbar-menu">
+                        <div className="navbar-start">
+                            <Link className={"navbar-item"} to="/">Home</Link>
+                        </div>
+
+                        <div className="navbar-end">
+                            <Link className={"navbar-item"} to="/login/">Se connecter</Link>
+                            <Link className={"navbar-item"} to="/register/">Creer un compte</Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
 
-export default LoginForm
+export default Navbar
