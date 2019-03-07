1e4ea3f3701674e9f37a25e2d1b0043f7dd8fd07
Baldrani <mael.mayon@free.fr>
2019-03-07 12:10:49 +0100

    Clean

--- client/src/components/AddGow.jsx
+++ client/src/components/AddGow.jsx
@@ -3,7 +3,32 @@ import React, {PureComponent} from 'react'
 class RegisterForm extends PureComponent {
     render() {
         return (
-            <div></div>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                this.props.onSubmit()}}
+            >
+                <div className="field">
+                    <label className="label" htmlFor="username">Username</label>
+                    <div className="control">
+                        <input className={"input"} type="text" name={"username"} id={"username"}
+                               onChange={(event) => this.props.onChange(event.currentTarget.value, "username")}/>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label" htmlFor="password">Password</label>
+                    <div className="control">
+                        <input className={"input"} type="password" name={"password"} id={"password"}
+                               onChange={(event) => this.props.onChange(event.currentTarget.value, "password")}/>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <div className="control">
+                        <input className="button is-link" type={"submit"}/>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
